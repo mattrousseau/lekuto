709edaf504a013c5b24a22731a8094f9a573dc7c
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-06-07 09:59:38 +0200

    baseline lin reg

--- lekuto/trainer.py
+++ lekuto/trainer.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
 
 class trainer():
     def train():
         ohe_encoder = OneHotEncoder()
+        lin_reg = LinearRegression()
